57c39c1a5c5ee4965fe6b678cc423b97f03ffc97
pyan83 <puyanwei@gmail.com>
2017-07-25 14:58:22 +0100

    Refactored code for fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,6 @@
 def fizzbuzz(number)
-    if number % 3 == 0 && number % 5 == 0
-      return 'fizzbuzz'
-    elsif number % 3 == 0
-        return 'fizz'
-    elsif number % 5 == 0
-        return 'buzz'
-    else number.to_s
-    end
+  return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
+  return 'fizz'if number % 3 == 0
+  return 'buzz' if number % 5 == 0
+  return number.to_s
 end
